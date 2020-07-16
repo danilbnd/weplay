6103d646bdac462fd897663bfa2313503063514f
danilbnd <danilbnd95@gmail.com>
2020-07-16 22:01:57 +0300

    add opacity

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
-const photoPlayer = gsap.timeline();
-photoPlayer.from(".db-player__image-bg", 1, { 
+const photoPlayer = TweenMax.from(".db-player__image-bg", 1, { 
 	delay: .4,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
+
 const namePlayerBg = TweenMax.from(".db-player__name", 1, { 
-	delay: 1,
+	delay: .8,
 	x: -20,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
+const strip = TweenMax.from(".db-player__strip", 2, { 
+	delay: 1.1,
+	x: 200,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
 const teamPlayer = TweenMax.from(".db-player__name-team", 1, { 
-	delay: 3,
+	delay: 2.3,
 	y: -20,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
 const nameText = TweenMax.from("#name-text", 1, { 
-	delay: 2,
-	y: -10,
+	delay: 1.9,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
 const nameTeamText = TweenMax.from("#name-team-text", 1, { 
-	delay: 4,
+	delay: 2.7,
 	x: -20,
 	opacity: 0,
 	ease: Expo.easeInOut
