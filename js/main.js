addf8c22f883dd13ae8c707ef87b2e73ee6a9a23
danilbnd <danilbnd95@gmail.com>
2020-07-16 21:16:18 +0300

    another animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 const photoPlayer = gsap.timeline();
-
-photoPlayer.from(".db-player__image-bg", {x: 1000, duration: 1});
-const namePlayer = TweenMax.from(".db-player__name", {y: 1000, x: -1000, duration: 1});
-const teamPlayer = TweenMax.from(".db-player__name-team", {y:1000, x: 1000, duration: 1});
+photoPlayer.from(".db-player__image-bg", 1, { 
+	delay: .4,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+const namePlayerBg = TweenMax.from(".db-player__name", 1, { 
+	delay: 1,
+	x: -20,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+const teamPlayer = TweenMax.from(".db-player__name-team", 1, { 
+	delay: 3,
+	y: -20,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+const nameText = TweenMax.from("#name-text", 1, { 
+	delay: 2,
+	y: -10,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+const nameTeamText = TweenMax.from("#name-team-text", 1, { 
+	delay: 4,
+	x: -20,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
