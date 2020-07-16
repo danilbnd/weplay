fad43acd87a1cd4246cef19ec56cc539214f6905
danilbnd <danilbnd95@gmail.com>
2020-07-16 22:37:03 +0300

    fix delay

--- js/main.js
+++ js/main.js
@@ -10,25 +10,25 @@ const namePlayerBg = TweenMax.from(".db-player__name", 1, {
 	opacity: 0,
 	ease: Expo.easeInOut
 });
-const strip = TweenMax.from(".db-player__strip", 2, { 
-	delay: 1.1,
+const strip = TweenMax.from(".db-player__strip", 1, { 
+	delay: .9,
 	x: 200,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
 const teamPlayer = TweenMax.from(".db-player__name-team", 1, { 
-	delay: 2.3,
+	delay: 1.5,
 	y: -20,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
 const nameText = TweenMax.from("#name-text", 1, { 
-	delay: 1.9,
+	delay: 1.4,
 	opacity: 0,
 	ease: Expo.easeInOut
 });
 const nameTeamText = TweenMax.from("#name-team-text", 1, { 
-	delay: 2.7,
+	delay: 1.9,
 	x: -20,
 	opacity: 0,
 	ease: Expo.easeInOut
